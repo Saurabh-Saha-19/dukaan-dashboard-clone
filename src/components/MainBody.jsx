d4db046c8c4e5234f031986ca1c317fe30586432
Saurabh Saha <vksaha99340@gmail.com>
2024-09-13 14:49:45 +0530

    Bug fixed with cards responsiveness

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -5,7 +5,7 @@ function MainBody() {
   return (
     <div className="p-3 sm:p-8 flex flex-col gap-6">
       <Overview />
-      <div className="grid grid-cols-1 gap-5 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
         <Cards type={"blueCard"} />
         <Cards type={"normal"} />
         <Cards type={"normal"} />
